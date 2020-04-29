0ec27fc056528ff7d4d3e9927cdf9d1f26513513
Mauro Solcia <mauro.solcia@gmail.com>
2020-04-29 21:22:44 +0200

    Updted Messages_it.dart

--- client/messages_it.dart
+++ client/messages_it.dart
@@ -181,7 +181,7 @@ const itClient = const <String, String> {
   mUnarchive: "Rimostra",
   mOR: "o",
   mDone: "Fatto",
-  mResendEmail: "Resend Email",
+  mResendEmail: "Reinvia Email",
   mSent: "Inviato",
   mRename: "Rinomina",
   mMove: "Move",
--- server/messages_it.dart
+++ server/messages_it.dart
@@ -86,7 +86,7 @@ const itServer = const <String, String> {
   mFooterLinkDownloadAndroidAPK: "Scarica l\' APK Android",
   mFooterLinkSecurity: "Sicurezza",
   mFooterLinkCategorySupport: "Supporto e Risorse",
-  mUserGuide: "Guide",
+  mUserGuide: "Guida",
   mFooterLinkDevelopers: "Sviluppatori & API",
   mFooterLinkCategoryCompany: "Azienda",
   mPost: "Invia",
