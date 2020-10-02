ba42d4dc3774f6531506afa548ab800fcfbc5f43
Haruka-N <48037093+Haruka-N@users.noreply.github.com>
2020-10-02 19:02:01 +0800

    Update messages_ja.dart

--- server/messages_ja.dart
+++ server/messages_ja.dart
@@ -995,7 +995,7 @@ Quireをご利用になったご感想をお聞かせください。
   mRemindTimeAt: "の",
   mCollapseAllSubtaskByDefault: 'デフォルトでは子タスクを折りたたむ',
   mAutoAssignToMe: '新しいタスクを自動的に自分に設定',
-  mStrikethroughCompletedTasks: 'Strikethrough completed tasks',
+  mStrikethroughCompletedTasks: '完了したタスクに取り消し線を引く',
   mDeveloperConsoleTitle: "開発者アプリ",
   mDeveloperConsoleIntro:
     'ご自身やチームがQuireをもっと便利に楽しめるアプリを開発しませんか。<br>'
