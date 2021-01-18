c8f003d4c674387f51e8b9f7a6a0ac9ba844a315
Mauro Solcia <mauro.solcia@gmail.com>
2021-01-18 20:45:57 +0100

    updated messages_it.dart

--- client/messages_it.dart
+++ client/messages_it.dart
@@ -370,7 +370,7 @@ const itClient = const <String, String> {
   mTaskArchiveNeverOption: "Non mostrare più",
   mTaskArchiveDayOption: "Mostra nuovamente [=n] giorno/i dopo",
   mTreeMenuArchiveTip: "Le attività <a href='[=link]' target='_blank'>Peekaboo</a> verranno nascosce fino a che non sei pronto per gestirle.",
-  mTreeMenuMarkAsRead: "Mark as read",
+  mTreeMenuMarkAsRead: "Segna come letto",
   mTreeBatchCompleteTooltip: "Completa",
   mTreeBatchAssigneeTooltip: "Assegna",
   mTreeBatchDateTooltip: "Data",
